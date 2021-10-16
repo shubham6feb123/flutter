fb2df4043a36e7c472d50d06e602ca5d8f19badb
shubham6feb123 <maarahuls555@gmail.com>
2021-10-16 17:51:29 +0530

    Scaffold learned

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter__app/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Container(
-          child: Text("Welcome to 30 days of flutter"),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
