a1f93f14a8bedee1c9a69f6d227ee20e267c8879
shubham6feb123 <maarahuls555@gmail.com>
2022-02-10 23:56:37 +0530

    Learned Scaffold widget

---
+++ lib/pages/home_Page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("App Bar"),
+        ),
+        body: Center(
+          child: Container(
+            child: Text("Flutter learning"),
+          ),
+        ),
+        drawer: Drawer(
+          backgroundColor: Colors.orange[700],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        selectedItemColor: Colors.amber[800],
+       
+      ),);
+  }
+}
